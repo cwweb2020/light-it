8c9e3a1ad492dbbacfe1bb29b9c33b55f0c3024a
claudio <bmx347@gmail.com>
2022-10-03 02:42:24 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import logo from '../assets/logo.jpeg'
+import down from '../assets/arrow-down.png'
+import {config} from '../utils/ui_config'
+
+const Header = () => {
+  console.log(down);
+  const navigate = useNavigate()
+
+  return (
+    <fieldset className='w-full h-28 bg-secondary'>
+      <div className='w-full h-full flex justify-between items-center px-32'>
+        <img src={logo} onClick={() => navigate('/')} className='w-48 cursor-pointer' alt="logo" />
+        <select className='header-select py-3 px-4 rounded-md' >
+        {config.header.map((item,index) => (<option key={index}>{item}</option>))}
+        </select>
+      </div>
+    </fieldset>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+    colors:{
+      primary:'#f7f7f7',
+      secondary:'#333333',
+      typography:'#707070',
+      sidebar_bg:'#ffffff'
+    }
+  },
+  plugins: [],
+}
